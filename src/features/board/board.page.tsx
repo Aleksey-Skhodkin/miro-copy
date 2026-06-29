408eda7d3969193f6c9af523b8e71df629b39a0c
Alexey Shodkin <C:\The Bat!>
2026-06-29 15:52:53 +0300

    arrow

--- src/features/board/board.page.tsx
+++ src/features/board/board.page.tsx
@@ -14,6 +14,7 @@ import { Overlay } from "./ui/Overlay";
 import { SelectionWindow } from "./ui/SelectionWindow";
 import { Sticker } from "./ui/Sticker";
 import { useViewModel } from "./view-model/use-view-model";
+import { vectorFromPoints, type Point } from "./domain/point";
 
 function BoardPage() {
   const nodesModel = useNodes();
@@ -53,6 +54,7 @@ function BoardPage() {
         {viewModel.selectionWindow && (
           <SelectionWindow {...viewModel.selectionWindow} />
         )}
+        <Arrow start={{ x: 30, y: 50 }} end={{ x: 90, y: 90 }} />
       </Canvas>
       <Actions>
         <ActionButton
@@ -70,3 +72,38 @@ function BoardPage() {
 }
 
 export const Component = BoardPage;
+
+function Arrow({ start, end }: { start: Point; end: Point }) {
+  const diff = vectorFromPoints(start, end);
+  const angle = Math.atan2(diff.y, diff.x);
+  const arrowRightAngle = angle + Math.PI * (1 - 1 / 6);
+  const arrowLeftAngle = angle - Math.PI * (1 - 1 / 6);
+  const arrowRightDiff = [
+    Math.cos(arrowRightAngle) * 10,
+    Math.sin(arrowRightAngle) * 10,
+  ];
+  const arrowLeftDiff = [
+    Math.cos(arrowLeftAngle) * 10,
+    Math.sin(arrowLeftAngle) * 10,
+  ];
+
+  return (
+    <svg className="absolute left-0 top-0 pointer-events-none">
+      <path
+        className="pointer-events-auto"
+        stroke="black"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        fill="black"
+        d={`
+          M ${start.x} ${start.y} L ${end.x} ${end.y} 
+          M ${end.x} ${end.y} L ${end.x + arrowRightDiff[0]} ${end.y + arrowRightDiff[1]} 
+          L ${end.x + -5 * Math.cos(angle)} ${end.y + -5 * Math.sin(angle)}
+          L ${end.x + arrowLeftDiff[0]} ${end.y + arrowLeftDiff[1]}
+          L ${end.x} ${end.y}
+          `}
+      />
+    </svg>
+  );
+}
